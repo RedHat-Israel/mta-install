db6c62b4bd2b056ac51ab1afc31afc761af8df92
Igor Braginsky <ibragins@redhat.com>
2025-11-20 19:51:45 +0200

    Updated to apply for 8.0.0 and fresh OCP version
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- 4_create_subscription.sh
+++ 4_create_subscription.sh
@@ -7,10 +7,10 @@ metadata:
   name: mta-operator
   namespace: openshift-mta
 spec:
-  channel: stable-v6.0
+  channel: stable-v8.0
   installPlanApproval: $approval
   name: mta-operator
   source: mta-to-upgrade
   sourceNamespace: openshift-marketplace
-  startingCSV: mta-operator.v6.0.0
+  startingCSV: mta-operator.v8.0.0
 EOF
--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@
 ./3_create_operator_group.sh
 ./4_create_subscription.sh Manual
 # sleep 1m
-oc patch ip $(oc get ip --all-namespaces|grep 'v6.0.0'|awk '{print $2}') -n openshift-mta --type merge --patch '{"spec":{"approved":true}}'
+#oc patch ip $(oc get ip --all-namespaces|grep 'v6.0.0'|awk '{print $2}') -n openshift-mta --type merge --patch '{"spec":{"approved":true}}'
+oc patch installplan $(oc get installplan --all-namespaces|grep -E 'v8.0.0|rhbk'|awk '{print $2}') -n openshift-mta --type merge --patch '{"spec":{"approved":true}}'
 ./5_create_tackle_cr.sh
